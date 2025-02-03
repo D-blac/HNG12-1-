07de3030c1a1fc2b396345f5ad5e2b69493c3729
D-blac <a.damian.ng.od@gmail.com>
2025-02-03 15:29:12 +0100

    API corrected

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const cors = require('cors');
 const { classifyNumber } = require('./controllers/classifyController');
 
 const app = express();
-const PORT = process.env.PORT || 7777;
+const PORT = process.env.PORT || 7777
+;
 
-// Enable CORS
+
 app.use(cors());
 
-// Root Endpoint (Optional)
 app.get('/', (req, res) => {
-    res.send('Welcome to the Number Classification API');
+    req.query.number = '371';
+    classifyNumber(req, res); 
 });
 
-// API Route
+
 app.get('/api/classify-number', classifyNumber);
 
-// 404 Error Handling for Undefined Routes
+
 app.use((req, res) => {
     res.status(404).json({ error: 'Endpoint not found' });
 });
