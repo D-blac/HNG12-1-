1883cb7efd6b5699d3e084f800a7e0a2a08b5b69
D-blac <a.damian.ng.od@gmail.com>
2025-02-03 15:37:39 +0100

    Fix error message for invalid input

--- controllers/classifyController.js
+++ controllers/classifyController.js
@@ -1,24 +1,20 @@
-const axios = require('axios');
-const { isPrime, isPerfect, isArmstrong, getParity, digitSum } = require('../utils/numberUtils');
-
 const classifyNumber = async (req, res) => {
     const number = req.query.number;
 
-    
+    // Input Validation
     if (!number || isNaN(number)) {
-        return res.status(400).json({ number, error: true });
+        return res.status(400).json({ number: "alphabet", error: true });
     }
 
     const num = parseInt(number);
     const properties = [];
     let funFact = '';
 
-   
+    // Check for Armstrong number
     const armstrong = isArmstrong(num);
     if (armstrong) {
         properties.push('armstrong');
 
-      
         const digits = num.toString().split('').map(Number);
         const power = digits.length;
         const calculation = digits.map(d => `${d}^${power}`).join(' + ');
@@ -26,7 +22,6 @@ const classifyNumber = async (req, res) => {
         funFact = `${num} is an Armstrong number because ${calculation} = ${num}`;
     } else {
         try {
-            // Fetch Fun Fact from Numbers API only if NOT Armstrong
             const response = await axios.get(`http://numbersapi.com/${num}/math?json`);
             funFact = response.data.text;
         } catch (error) {
